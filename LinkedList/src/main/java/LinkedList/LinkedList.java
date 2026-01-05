038a2df47054071616474c23564e582ca7092ac6
Mergen Bauyrzhan <mambablackright@gmail.com>
2026-01-06 04:26:31 +0500

    [Linked List] Full implementation of linked list was added

--- LinkedList/src/main/java/LinkedList/LinkedList.java
+++ LinkedList/src/main/java/LinkedList/LinkedList.java
@@ -1,95 +1,186 @@
 package LinkedList;
 
-public class LinkedList<Type> {
+public class LinkedList<T> {
 	private int size = 0;
-	private Node tailNode;
-	private Node headNode;
-	public static class Node<Type> {
-		Type value;
-		Node nextNode;
-		public Node (Type value) {
+
+	public static class Node<T> {
+		T value;
+		Node<T> nextNode;
+		public Node (T value) {
 			this.value = value;
 			nextNode = null;
-		}
+		} 
 	}
-	public void insertAtHead (Type n) {
-		Node<Type> newNode = new Node<Type>(n);			
-		if (size == 0) {
-			headNode = newNode;
-			tailNode = newNode;
-			size++;
-		} else {
-			newNode.nextNode = headNode;
-			headNode = newNode;
-			size++;
-		}
+	
+	Node<T> headNode;
 
-	}
-	public void insertAtTail (Type n) {
-		Node<Type> newNode = new Node<Type>(n);
-		if (size == 0) {
-			headNode = newNode;
-			tailNode = newNode;
-			size++;
-		} else {
-			tailNode.nextNode = newNode;
-			tailNode = newNode;
-			size++;
-		}
+	public boolean isEmpty () {
+		return size == 0;
 	}
 
-	public void insertAtIndex (Type n, int index) {
-		Node<Type> newNode = new Node<Type>(n);
-		if (index > size) System.out.println("ERROR");
+	public String allValuesToString () {
+		Node<T> currentNode = headNode;
+		String result = "";
+		while (currentNode.nextNode != null) {
+			result += currentNode.value.toString() + " ";
+			currentNode = currentNode.nextNode;
+		}
+		result += currentNode.value.toString();
+		return result;
+	}
+
+	public LinkedList () {
+		headNode = null;	
+	}
+	
+	public void insertAtHead (T value) {
+		Node<T> newNode = new Node<T>(value);
+		newNode.nextNode = headNode;
+		headNode = newNode;
+		size++;
+	}
+	
+	public void insertAtTail (T value) {
+		Node<T> newNode = new Node<T>(value);
+		if (isEmpty()) {
+			// i could have used insertAtHead() here but since it also 
+			// increments size each time it is called, it would have result 
+			// in double increment if this method is called once on an empty 
+			// linked list. you might ask "why you did not put size increment 
+			// of this method in else block then?", 
+			// well it is because i like to keep size increment at the end, 
+			// it is like cherry on top for me. 
+			// just because i used this exact way of adding at tail if the linked list is empty, 
+			// it does not mean that i consider it a superior way :)
+			headNode = newNode;
+		} else {
+			Node<T> currentNode = headNode;
+			while (currentNode.nextNode != null) {
+				currentNode = currentNode.nextNode;
+			}
+			currentNode.nextNode = newNode;
+		}
+		size++;	
+	}
+	
+	public void insertAtIndex(T value, int index) throws LinkedListException {
+		// these two conditions are error conditions therefore they must not increment the size value
+		if (index + 1 > size() || index < 0) throw new LinkedListException("index is out of boundaries");
 		else {
-			Node current = headNode;
 			int counter = 0;
+			Node<T> currentNode = headNode;
 			while (counter != index - 1) {
-				current = current.nextNode;
+				currentNode = currentNode.nextNode;
 				counter++;
 			}
-			Node temp = current.nextNode;
-			current.nextNode = newNode;
+			Node<T> temp = currentNode.nextNode;
+			Node<T> newNode = new Node<T>(value);
 			newNode.nextNode = temp;
+			currentNode.nextNode = newNode;
 			temp = null;
 			size++;
 		}
-	}
-
-
-	public void deleteAtHead() {
-		if (size == 0) System.out.println("ERROR");
-   		Node temp = headNode;
-		headNode = headNode.nextNode;
-		temp.nextNode = null;
-	}
-
-	public void deleteAtTail() {
-		if (size == 0) System.out.println("ERROR");
-		else {
-			Node current = headNode;
 		
-			while (current.nextNode != tailNode) {
-				current = current.nextNode;
+	}
+	
+	public T deleteAtHead () throws LinkedListException {
+		if (isEmpty()) { 
+ 			throw new LinkedListException("cannot delete from empty linked list");
+		}
+		else {
+			Node<T> deletedNode = headNode;
+			headNode = headNode.nextNode;
+			deletedNode.nextNode = null;
+			size--;	
+			return deletedNode.value;
+		}	
+	}	
+	
+	public T deleteAtTail () throws LinkedListException {
+		if (isEmpty()) throw new LinkedListException("cannot delete from empty linked list");
+		else if (size() == 1) {
+			Node<T> deletedNode = headNode;
+			headNode = null;
+			size--;
+			return deletedNode.value;
+		}
+		else {
+			Node<T> currentNode = headNode;
+			while (currentNode.nextNode.nextNode != null) {
+				currentNode = currentNode.nextNode;
 			}
-			current.nextNode = null;
+			Node<T> deletedNode = currentNode.nextNode;
+			currentNode.nextNode = null;
+
+			// since it is a tail node, it already points to null, hence won't have any pointer to our 
+			// linked list, therefore we don't have to explicitly make the tail node equal null
+
+			size--;
+			return deletedNode.value;
+		}
+	}
+	
+	public T deleteAtIndex (int index) throws LinkedListException {
+		if (isEmpty()) throw new LinkedListException("cannot delete from empty linked list");
+		else if (index < 0 || index >= size()) throw new LinkedListException("given index is out boundaries");
+		else {
+			if (index == 0) {
+				return deleteAtHead();
+			} else {
+				int counter = 0;
+				Node<T> currentNode = headNode;
+				while (counter != index - 1) {
+					currentNode = currentNode.nextNode;
+					counter++;
+				}
+				Node<T> deletedNode = currentNode.nextNode; 
+				currentNode.nextNode = currentNode.nextNode.nextNode;
+				deletedNode.nextNode = null;
+				size--;
+				return deletedNode.value;
+			}
 		}
 	}
 
-	public static void main (String[] args) {
-		LinkedList<Integer> ll = new LinkedList<>();
-		ll.insertAtTail(1);
-		ll.insertAtTail(2);
-		ll.insertAtTail(3);
-		ll.insertAtTail(4);
-		ll.deleteAtTail();
-		ll.deleteAtTail();
-		Node current = ll.headNode;
-		while (current != null) {
-			System.out.println(current.value);
-			current = current.nextNode;
-		}
+	public int size () {
+		return size;
 	}
+	
+	public T get(int index) throws LinkedListException {
+		if (isEmpty()) throw new LinkedListException("cannot get from empty linked list");	
+		else if (index < 0 || index >= size) throw new LinkedListException("given index is out of boundaries");
+		else {
+			Node<T> currentNode = headNode;
+			int counter = 0;
+			while (index != counter) {
+				currentNode = currentNode.nextNode;
+				counter++;
+			}
+			return currentNode.value;
+		}
+
+	}
+	
+	public boolean search (T value) {
+		
+		if (isEmpty()) throw new LinkedListException("cannot search in empty linked list");	
+		else {
+			Node<T> currentNode = headNode;
+			while (currentNode != null) {
+				if (currentNode.value == value) return true;
+				currentNode = currentNode.nextNode;
+			}
+		}
+		return false;
+	}
+	
+	
 
 
+}
+
+class LinkedListException extends RuntimeException {
+	public LinkedListException (String errorMessage) {
+		super(errorMessage);
+	}
 }
